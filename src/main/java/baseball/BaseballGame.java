d61e622eee9b131d0b3042a6ae0451c39e0fbc80
deok-beom <baek940dog@gmail.com>
2023-11-08 16:53:56 +0900

    Feat: 히스토리를 이용한 게임 결과 표시 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -3,10 +3,12 @@ package baseball;
 import util.Terminal;
 
 import java.io.IOException;
+import java.util.List;
 
 public class BaseballGame {
     public static void main(String[] args) throws IOException {
         while (true) {
+            Terminal.out("게임 시작!" + System.lineSeparator());
             Number targetNumber = RandomNumberGenerator.generate();
             BaseballStage stage = new BaseballStage(targetNumber);
             if (proceed(stage)) {
@@ -19,14 +21,19 @@ public class BaseballGame {
         while (true) {
             boolean proceed = stage.proceed();
             if (proceed) {
-                return askPlayEnd();
+                return askPlayEnd(stage.getHistory());
             }
         }
     }
 
-    private static boolean askPlayEnd() throws IOException {
+    private static boolean askPlayEnd(List<Discrimination> history) throws IOException {
         while (true) {
-            Terminal.out("세 개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            Terminal.out("세 개의 숫자를 모두 맞히셨습니다! 게임 종료" + System.lineSeparator());
+
+            for (Discrimination discrimination : history) {
+                Terminal.out(discrimination.show());
+            }
+
             String in = Terminal.in("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요." + System.lineSeparator());
             if (in.trim().equals("1")) {
                 return false;
--- src/main/java/baseball/Discrimination.java
+++ src/main/java/baseball/Discrimination.java
@@ -5,45 +5,73 @@ import java.util.Map;
 import java.util.stream.IntStream;
 
 public class Discrimination {
+    private final int input;
     private final int strike;
     private final int ball;
     private final int out;
+    private final String[] visual = new String[3];
 
     public Discrimination(Number base, Number compare) {
-        int[] baseArr = base.get();
         int[] compareArr = compare.get();
+        int input = 0;
+        for (int i : compareArr) {
+            input = input * 10 + i;
+        }
+        this.input = input;
 
-        Map<Integer, Integer> compareMap = new HashMap<>();
-        IntStream.range(0, 3).forEach(i -> compareMap.put(compareArr[i], i));
+        int[] baseArr = base.get();
+        Map<Integer, Integer> baseMap = new HashMap<>();
+        IntStream.range(0, 3).forEach(i -> baseMap.put(baseArr[i], i));
 
         int strike = 0;
         int ball = 0;
 
-        for (int i = 0; i < baseArr.length; i++) {
-            if (compareMap.containsKey(baseArr[i])) {
-                if (compareMap.get(baseArr[i]) == i) {
+        for (int i = 0; i < compareArr.length; i++) {
+            if (baseMap.containsKey(compareArr[i])) {
+                if (baseMap.get(compareArr[i]) == i) {
+                    visual[i] = "S";
                     strike++;
                 } else {
+                    visual[i] = "B";
                     ball++;
                 }
 
-                compareMap.remove(baseArr[i]);
+                baseMap.remove(baseArr[i]);
+            } else {
+                visual[i] = "O";
             }
         }
 
         this.strike = strike;
         this.ball = ball;
-        this.out = compareMap.size();
+        this.out = baseMap.size();
     }
 
     public String print() {
         StringBuilder sb = new StringBuilder();
-        if (getBall() > 0) {
+        sb.append(String.format("[입력:%d] > ", this.input));
+
+        if (ball > 0) {
             sb.append(String.format("%d볼 ", getBall()));
         }
 
-        if (getStrike() > 0) {
+        if (strike > 0) {
             sb.append(String.format("%d스트라이크", getStrike()));
+        }
+
+        if (out == 3) {
+            sb.append("3아웃!");
+        }
+
+        sb.append(System.lineSeparator());
+        return sb.toString();
+    }
+
+    public String show() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("[입력:%d] > ", this.input));
+        for (String s : visual) {
+            sb.append(s);
         }
 
         sb.append(System.lineSeparator());
